2c6548dfa95b83dce306f6fd30871e48d3a68748
MatiBrego <matias.bregoli@ing.austral.edu.ar>
2023-04-10 13:38:07 -0300

    Added tests for edge cases for Follow Service. Updated Follow Service to handle them correctly.

--- node/express-prisma-typescript/__tests__/domains/follow/service/follow.service.test.ts
+++ node/express-prisma-typescript/__tests__/domains/follow/service/follow.service.test.ts
@@ -1,11 +1,11 @@
 import {FollowServiceImpl} from "../../../../src/domains/follow/service";
 import {FollowRepositoryImpl} from "../../../../src/domains/follow/repository/follow.repository.impl";
-import {db} from "../../../../src/utils";
+import {db, NotFoundException} from "../../../../src/utils";
 import {createUsersInDb} from "../../../../src/testPreparation";
 
 let users: {username: string, email: string, password: string, id: string}[] = [];
 
-beforeAll(async () => {
+beforeEach(async () => {
     //Delete all users
     await db.user.deleteMany({})
 
@@ -20,7 +20,10 @@ describe("Test Follow Service", () => {
         const user1 = users[0];
         const user2 = users[1];
 
+        // User 1 follows user 2
         const result = await followService.FollowUser(user1.id, user2.id)
+
+        // Search Follow registry in the db
         const dbCall = await db.follow.findFirst({where: {followerId: user1.id, followedId: user2.id}})
 
         expect(dbCall?.id).toBe(result.id);
@@ -30,9 +33,41 @@ describe("Test Follow Service", () => {
         const user1 = users[0];
         const user2 = users[1];
 
+        // User 1 follows user 2
+        await followService.FollowUser(user1.id, user2.id)
+
+        // User 1 unfollows user 2
         await followService.UnfollowUser(user1.id, user2.id)
+
+        // Search Follow registry in the db
         const dbCall = await db.follow.findFirst({where: {followerId: user1.id, followedId: user2.id}})
 
         expect(dbCall).toBe(null);
+    })
+
+    test("GivenAUser_WhenTryingFollowThemselves_ThenAnErrorShouldBeThrown", async () => {
+        const user1 = users[0];
+
+        // Make user1 follow himself
+        await expect(async () => await followService.FollowUser(user1.id, user1.id)).rejects.toThrow(Error)
+    })
+
+    test("GivenAUserThatFollowsAnother_WhenTryingToFollowTheSameUserAgain_ThenAnErrorShouldBeThrown", async () => {
+        const user1 = users[0];
+        const user2 = users[1];
+
+        // User 1 follows user 2
+        await followService.FollowUser(user1.id, user2.id)
+
+        // Try to follow again
+        await expect(async () => await followService.FollowUser(user1.id, user2.id)).rejects.toThrow(Error)
+    })
+
+    test("GivenTwoUsersThatDoNotFollowEachOther_WhenTryingToUnfollow_ThenAnErrorShouldBeThrown", async () => {
+        const user1 = users[0];
+        const user2 = users[1];
+
+        // Try to unfollow user 2
+        await expect(async () => await followService.UnfollowUser(user1.id, user2.id)).rejects.toThrow(new NotFoundException('Follower'))
     })
 })
--- node/express-prisma-typescript/src/domains/follow/service/follow.service.impl.ts
+++ node/express-prisma-typescript/src/domains/follow/service/follow.service.impl.ts
@@ -2,7 +2,7 @@ import {FollowService} from "@domains/follow/service/follow.service";
 import {FollowDto} from "@domains/follow/dto";
 import {FollowRepository} from "@domains/follow/repository/follow.repository";
 
-import {NotFoundException} from "@utils";
+import {NotFoundException} from "../../../utils";
 
 export class FollowServiceImpl implements FollowService{
 
@@ -10,7 +10,14 @@ export class FollowServiceImpl implements FollowService{
     }
 
     async FollowUser(followerId: string, followedId: string): Promise<FollowDto> {
-           return await this.repository.create(followerId, followedId)
+        // Check if followed is the same as follower
+        if(followedId === followerId) {throw new Error("Follower id and followed id cannot be the same")}
+
+        // Check if Follow already exists
+        const exists = await this.repository.getByUsersId(followerId, followedId);
+        if(exists) {throw new Error("Follower already follows that user")}
+
+        return await this.repository.create(followerId, followedId)
     }
 
     async UnfollowUser(followerId: string, followedId: string): Promise<void> {
